c9ddb1be4b53f7685cf8f599cd9e390f8e78c6c5
kannanrr54 <kannanrr54@gmail.com>
2022-02-07 10:42:34 +0530

    Update 2.py

--- 2/2.py
+++ 2/2.py
@@ -1,11 +1,11 @@
-f=open("input.txt","r")
+f=open("2-input.txt","r")
 print("Actual file")
 print("")
 for i in f.readlines():
         print(i.strip())
 f.close()
-f=open("input.txt","r")
-o=open("output.txt","w")
+f=open("2-input.txt","r")
+o=open("2-output.txt","w")
 for c in f.readlines():
     if(len(c.split(" "))>5 and c[0].isupper()):
         o.write(c)
@@ -14,7 +14,7 @@ o.close()
 print("")
 print("Output--------------------------------")
 print("")
-o=open("output.txt","r")
+o=open("2-output.txt","r")
 for c in o.readlines():
     print(c.strip())
 o.close()
